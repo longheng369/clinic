7171dd10041e118a7fa2490bc99b3a12f4014a56
Longheng Chhoeun <longheng@web-essentials.co>
2026-09-16 14:58:22 +0700

    [IMP] Improve para clinic module
    refs CLS-00

--- resources/js/components/form/multiAutocomplete.tsx
+++ resources/js/components/form/multiAutocomplete.tsx
@@ -23,7 +23,6 @@ type Props<T extends FieldValues = FieldValues> = {
   renderOption?: any;
   getOptionLabel?: any;
   renderValue?: any;
-  getChipLabel?: (option: Option) => string;
 };
 
 const MultiAutocomplete = <T extends FieldValues = FieldValues>({
@@ -37,24 +36,11 @@ const MultiAutocomplete = <T extends FieldValues = FieldValues>({
   renderOption,
   getOptionLabel,
   renderValue,
-  getChipLabel,
 }: Props<T>) => {
   const { field, fieldState } = useController({ control, name, rules });
 
   const selectedOptions = options.filter((opt) =>
     (field.value as (number | string)[]).includes(opt.value),
-  );
-
-  const defaultRenderValue = (selected: any) => (
-    <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
-      {selected.map((option: any) => (
-        <Chip
-          key={option.value}
-          label={getChipLabel ? getChipLabel(option) : option.label}
-          size="small"
-        />
-      ))}
-    </Box>
   );
 
   return (
@@ -68,13 +54,11 @@ const MultiAutocomplete = <T extends FieldValues = FieldValues>({
       isOptionEqualToValue={(option: any, value: any) => option.value === value.value}
       onChange={(_: any, value: any) => field.onChange(value.map((opt: any) => opt.value))}
       noOptionsText="No results found."
-      size="small"
       renderOption={renderOption}
-      renderValue={renderValue ?? defaultRenderValue}
+      renderValue={renderValue}
       renderInput={(params) => (
         <TextField
           {...params}
-          variant="standard"
           label={label}
           placeholder={placeholder}
           required={!!rules?.required}
